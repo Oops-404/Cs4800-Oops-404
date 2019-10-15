2afd9b9dd244e6e40855ab699b20da81c42b90aa
Annie Wu <anniewu2303@gmail.com>
2019-10-14 20:00:02 -0700

    Case-insensitive search for events by location and name

--- PlanEasy/src/main/java/cs4800/database/Database.java
+++ PlanEasy/src/main/java/cs4800/database/Database.java
@@ -16,21 +16,26 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 /*
- * Database class
+ * Database class contains all functions regarding queries and 
+ * manipulation of data in the Oops404 database.
  * 
  */
 public class Database {
 	
 	private static final Logger log = Logger.getLogger(Database.class.getName());
 
-	private static String URI = "mongodb+srv://admin:Oops!404@oops404-ijzpy.azure.mongodb.net/test?retryWrites=true&w=majority";
+	private static String URI = "mongodb+srv://admin:Oops!404@oops404-ijzpy.azure.mongodb.net/Event?retryWrites=true&w=majority";
 	private static MongoClient mongoClient = null;
 	private static MongoDatabase db = null;
+	
+	private static MongoCollection<Document> event = null;
+	private static MongoCollection<Document> user = null;
 	
 	public Database() {	}
 	
 	/**
-	 * Initialize connection to MongoDB server and access Oops404 database
+	 * Initialize connection to MongoDB server, access Oops404 database, 
+	 * and initialize each collection in the database
 	 * 
 	 */
 	public void connect() {
@@ -39,6 +44,8 @@ public class Database {
 			mongoClient = new MongoClient(new MongoClientURI(URI));
 			log.info("Connected to MongoDB :)");
 			db = mongoClient.getDatabase("Oops404");
+			event = db.getCollection("Event");
+			user = db.getCollection("User");
 		} 
 		
 		catch (MongoException e) {
@@ -47,29 +54,58 @@ public class Database {
 	}
 	
 	/**
-	 * Query Events by their location
-	 * @param enterLocation location entered by user
+	 * Query event by their location
+	 * @param userEntered string entered by user
 	 */
-	public void selectEventByLocation(String enterLocation) {
+	public void selectEventByLocation(String userEntered) {
 		
-		log.info("User has entered: " + enterLocation);
+		log.info("User has entered: " + userEntered);
 		
-		log.info("Getting Events collection");
-		MongoCollection<Document> events = db.getCollection("Event");
+		log.info("Getting Event collection...");
+		MongoCollection<Document> event = db.getCollection("Event");
 
-        log.info("Fetching all records in the test collection where location is " + enterLocation);
-        FindIterable<Document> find = events.find(eq("location", enterLocation));
-        MongoCursor<Document> cursor = find.iterator();
+        log.info("Fetching all records in the Event collection where location contains: " + userEntered);
+        // find(regex("field name", "pattern", "options"));
+        FindIterable<Document> results = event.find(regex("location", userEntered, "i")); // i for case-insensitive
+        MongoCursor<Document> cursor = results.iterator();
         
         try {
             while(cursor.hasNext()) {
-            	// print in Json format
+            	// print results in Json format
                 log.info(cursor.next().toJson());
             }
-        } finally {
+        } 
+        finally {
             cursor.close();
-            log.info("End of records");
+            log.info("End of records where event location contains: " + userEntered);        
         }
+	}
+	
+	/**
+	 * Query Event event by what the event name contains
+	 * @param userEntered string entered by user
+	 */
+	public void selectEventByNameEvent(String userEntered) {
+		
+		log.info("User has entered: " + userEntered);
+		log.info("Getting Event collection...");
+
+        log.info("Fetching all records in the Event collection where event name contains: " + userEntered);
+        // find(regex("field name", "pattern", "options"));
+        FindIterable<Document> results = event.find(regex("name", userEntered, "i")); // i for case-insensitive
+        
+        MongoCursor<Document> cursor = results.iterator();
+        
+        try {
+            while(cursor.hasNext()) {
+            	// print results in Json format
+                log.info(cursor.next().toJson());
+            }
+        }
+        finally {
+            cursor.close();
+            log.info("End of records where event name contains: " + userEntered);
+        }     
 	}
 	
 	public static void main(String[] args) {
