3ccf1981beedbe1b69ff52e6e15f5d69c5eb98e8
Annie Wu <anniewu2303@gmail.com>
2019-10-27 16:46:38 -0700

    event

--- PlanEasy/src/main/java/cs4800/event/Event.java
+++ PlanEasy/src/main/java/cs4800/event/Event.java
@@ -4,9 +4,9 @@ import java.time.LocalDate;
 import java.time.Month;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
