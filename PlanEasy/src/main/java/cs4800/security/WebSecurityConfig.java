b1a9e934a6fc63aaeca24a41d2abfe37696a82a6
Adam Johns <awjohns@cpp.edu>
2019-11-06 13:22:57 -0800

    Disabled security for testing purposes

--- PlanEasy/src/main/java/cs4800/security/WebSecurityConfig.java
+++ PlanEasy/src/main/java/cs4800/security/WebSecurityConfig.java
@@ -60,10 +60,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	}
 
+	//TODO security is disabled via ".antMatchers("/**").permitAll()" Line 68
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	    http.httpBasic().disable().csrf().disable().sessionManagement()
 	            .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+	            .antMatchers("/**").permitAll()
 	            .antMatchers("/auth/login").permitAll()
 	            .antMatchers("/auth/register").permitAll()
 	            .antMatchers("/admintest").hasAuthority("ADMIN").anyRequest().authenticated()
