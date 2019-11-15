40a335ed987e1a975899ea194204a1c81b7488a2
NickSoultanian <nicholas.soultanian@gmail.com>
2019-11-14 19:58:25 -0800

    first itteration
    tomorrow we kill it

--- react-planeasily/src/views/Calendar/Calendar.js
+++ react-planeasily/src/views/Calendar/Calendar.js
@@ -1,13 +1,6 @@
 import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-// core components
-import GridItem from "components/Grid/GridItem.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import Table from "components/Table/Table.js";
-import Card from "components/Card/Card.js";
-import CardHeader from "components/Card/CardHeader.js";
-import CardBody from "components/Card/CardBody.js";
 
 const styles = {
   cardCategoryWhite: {
@@ -40,10 +33,53 @@ const styles = {
 };
 
 const useStyles = makeStyles(styles);
+//const classes = useStyles();
 
-export default function Calendar() {
-  const classes = useStyles();
-  return (
-    <p>Something</p>
-  );
+class Calendar extends React.Component{
+  constructor(props) {
+    super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
+    this.state = { //state is by default an object
+      students: [
+        { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+        { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
+        { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
+        { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
+      ]
+    }
+  }
+  renderTableData() {
+    return this.state.students.map((student, index) => {
+      const { id, name, age, email } = student //destructuring
+      return (
+          <tr key={id}>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{age}</td>
+            <td>{email}</td>
+          </tr>
+      )
+    })
+  }
+  renderTableHeader() {
+    let header = Object.keys(this.state.students[0])
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>
+    })
+  }
+
+  render() {
+    return (
+        <div>
+          <h1 id='title'>React Dynamic Table</h1>
+          <table id='students'>
+            <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+            {this.renderTableData()}
+            </tbody>
+          </table>
+        </div>
+    )
+  }
+
 }
+export default Calendar;
